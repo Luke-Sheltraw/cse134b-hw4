26045265016bd4adecf3b0aad8c2223fc49230cb
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-11-25 16:06:02 -0800

    built out settings menu toggle

--- scripts/color-theme.js
+++ scripts/color-theme.js
@@ -1,7 +1,11 @@
 function syncColorThemeFromStorage() {
   const themeSwitcherEl = document.querySelector('#theme-switcher');
 
-  const currentTheme = window.localStorage.getItem('color-theme') ?? 'light';
+  const currentTheme = window.localStorage.getItem('color-theme') ?? (
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark' 
+      : 'light'
+  );
 
   if (currentTheme === 'dark') {
     themeSwitcherEl.checked = true;
@@ -22,10 +26,22 @@ function syncColorThemeToStorage() {
   window.localStorage.setItem('color-theme', currentTheme);
 }
 
+function toggleThemeSettingsMenu() {
+  const themeSettingsMenuEl = document.querySelector('dialog#settings-menu');
+
+  if (themeSettingsMenuEl.hasAttribute('open')) {
+    themeSettingsMenuEl.removeAttribute('open');
+  } else {
+    themeSettingsMenuEl.setAttribute('open', '');
+  }
+}
+
 function initializeColorThemeListeners() {
   const themeSwitcherEl = document.querySelector('#theme-switcher');
+  const themeSettingsBtnEl = document.querySelector('#settings-button');
 
   themeSwitcherEl.addEventListener('change', syncColorThemeToStorage);
+  themeSettingsBtnEl.addEventListener('click', toggleThemeSettingsMenu);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
